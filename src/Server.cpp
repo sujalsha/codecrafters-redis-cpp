b15cd93f76c6bb9474d83822d48034c607e036d4
sujalsha <sujal4345@gmail.com>
2024-07-23 03:26:20 +0530

    modified:   src/Server.cpp

--- src/Server.cpp
+++ src/Server.cpp
@@ -7,7 +7,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-using namespace std:
+using namespace std;
 
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
