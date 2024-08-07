f288e38e0adba3d708dd9b4991711b90d4170478
sujalsha <sujal4345@gmail.com>
2024-08-08 02:18:35 +0530

    modified: sec/Server.cpp

--- src/Server.cpp
+++ src/Server.cpp
@@ -7,81 +7,72 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-
+#define BUFFER_SIZE 128
 // Use the standard namespace to simplify code
 using namespace std;
 
+void handle(int fd)
+{ 
+  char buff[BUFFER_SIZE] = "";
+  bzero(&buff,sizeof(buff));
+  while(1)
+  { 
+    memset(&buff,'\0',sizeof(buff));
+    recv(fd, buff, 15, 0);
+    if(strcasecmp(buff,"*1\r\n$4\r\nping\r\n") !=0 )
+    {  
+      bzero(&buff,sizeof(buff));
+      continue;
+    }
+    send(fd, "+PONG\r\n",7, 0);
+  }
+  return ;
+}
 int main(int argc, char **argv) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    cout << "Logs from your program will appear here!\n";
-
-    // Uncomment this block to pass the first stage
-
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        cerr << "Failed to create server socket\n";
-        return 1;
-    }
-
-    // Since the tester restarts your program quite often, setting REUSE_PORT
-    // ensures that we don't run into 'Address already in use' errors
-    int reuse = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
-        cerr << "setsockopt failed\n";
-        return 1;
-    }
-
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(6379);
-
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) {
-        cerr << "Failed to bind to port 6379\n";
-        return 1;
-    }
-
-    int connection_backlog = 5;
-    if (listen(server_fd, connection_backlog) != 0) {
-        cerr << "listen failed\n";
-        return 1;
-    }
-
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
-
-    cout << "Waiting for a client to connect...\n";
-
-    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-    if (client_fd < 0) {
-        cerr << "Failed to accept client connection\n";
-        return 1;
-    }
-    cout << "Client connected\n";
-
-    // Use a buffer to store received messages
-    char msg[100];
-
-    // Loop to handle client messages
-    while (true) {
-        ssize_t bytes_received = recv(client_fd, msg, sizeof(msg) - 1, 0);
-        if (bytes_received == -1) {
-            cerr << "Failed to receive message\n";
-            return 1;
-        }
-
-        // Null-terminate the received data
-        msg[bytes_received] = '\0';
-
-        cout << "Received message: " << msg << endl;
-
-        const char *response = "+PONG\r\n";
-        if (send(client_fd, response, strlen(response), 0) == -1) {
-            cerr << "Failed to send message\n";
-            return 1;
-        }
-    }
-
-    close(server_fd);
-    return 0;
+  // You can use print statements as follows for debugging, they'll be visible when running tests.
+  // std::cout << "Logs from your program will appear here!\n";
+  // Uncomment this block to pass the first stage
+  //
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0) {
+   cerr << "Failed to create server socket\n";
+   return 1;
+  }
+  //
+  // Since the tester restarts your program quite often, setting SO_REUSEADDR
+  // ensures that we don't run into 'Address already in use' errors
+  int reuse = 1;
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+    cerr << "setsockopt failed\n";
+    return 1;
+  }
+  //
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(6379);
+  
+  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+    cerr << "Failed to bind to port 6379\n";
+    return 1;
+  }
+  
+  int connection_backlog = 5;
+  if (listen(server_fd, connection_backlog) != 0) {
+    cerr << "listen failed\n";
+    return 1;
+  }
+  
+  struct sockaddr_in client_addr;
+  int client_addr_len = sizeof(client_addr);
+  
+  cout << "Waiting for a client to connect...\n";
+  
+  int clientFd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  cout << "Client connected\n";
+  
+  handle(clientFd);
+  
+  close(server_fd);
+  return 0;
 }
